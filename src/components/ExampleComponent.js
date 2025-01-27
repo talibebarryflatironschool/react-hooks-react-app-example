60dfc751731045c7dc1a14a9726b4ecd50c5ad0e
Mamoudou Barry <barrymamoudou982@gmail.com>
2025-01-27 11:33:30 -0500

    completed

--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -1,16 +1,36 @@
 import React from "react";
 
-// no need to modify anything in here. The second test is looking for the returned JSX from this component
 function ExampleComponent() {
   return (
-    <div className="example">
+    <div>
+      <h3>Whoa!</h3>
       <img
-        src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
-        alt="a deeply impressed man, perhaps thinking 'whoa'"
+        src="https://media.giphy.com/media/3o7aD2saalBwwftBIY/giphy.gif"
+        alt="Sunglasses GIF"
       />
-      <p>Whoa!</p>
     </div>
   );
 }
 
 export default ExampleComponent;
+
+
+
+
+
+// import React from "react";
+
+// // no need to modify anything in here. The second test is looking for the returned JSX from this component
+// function ExampleComponent() {
+//   return (
+//     <div className="example">
+//       <img
+//         src="https://s3.amazonaws.com/ironboard-learn/sunglasses.gif"
+//         alt="a deeply impressed man, perhaps thinking 'whoa'"
+//       />
+//       <p>Whoa!</p>
+//     </div>
+//   );
+// }
+
+// export default ExampleComponent;
--- src/components/TestComponent.js
+++ src/components/TestComponent.js
@@ -1,20 +1,48 @@
 import React from "react";
 
-// no need to modify anything in here. The final test is looking for the returned JSX from this component
 function TestComponent() {
   return (
-    <div className="video">
-      <iframe
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/5TbUxGZtwGI"
-        frameBorder="0"
-        allow="autoplay; encrypted-media"
-        title="time video"
-        allowFullScreen
-      ></iframe>
+    <div>
+      <h3>This is the TestComponent!</h3>
+      <video title="time video" controls>
+        <source
+          src="https://www.w3schools.com/html/mov_bbb.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
     </div>
   );
 }
 
 export default TestComponent;
+
+
+
+
+
+
+
+
+
+
+// import React from "react";
+
+// // no need to modify anything in here. The final test is looking for the returned JSX from this component
+// function TestComponent() {
+//   return (
+//     <div className="video">
+//       <iframe
+//         width="560"
+//         height="315"
+//         src="https://www.youtube.com/embed/5TbUxGZtwGI"
+//         frameBorder="0"
+//         allow="autoplay; encrypted-media"
+//         title="time video"
+//         allowFullScreen
+//       ></iframe>
+//     </div>
+//   );
+// }
+
+// export default TestComponent;
